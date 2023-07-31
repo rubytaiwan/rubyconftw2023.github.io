2d33961261731d4ce85fa5d3a6f8671d9b4dfb38
ryudoawaru <ryudoawaru@users.noreply.github.com>
2023-07-31 06:37:35 +0000

    deploy: d0fc376a3efb985355cc3b61a9244fab9fd2d601

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  darkMode: "class",
+  content: [
+      "./_drafts/**/*.{html,md}",
+      "./_posts/**/*.{html,md}",
+      "./_includes/**/*.{html,md}",
+      "./_layouts/**/*.{html,md}",
+      "./*.{html,md}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'dark-pink': '#d90845',
+        'darkblue': '#000929',
+        'black': '#111111',
+      },
+      fontFamily: {
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans],
+        russo: ["Russo One", "sans-serif"]
+      },
+      fontSize: {
+        '2.5': '0.625rem', // 10px
+        '3.75': '0.9375rem', // 15px
+        '5.5': '1.375rem', // 22px
+        '6.5': '1.625rem', // 26px
+        '7.5': '1.875rem', // 30px
+        '8.5': '2.125rem', //34px
+        '9': '2.25rem', // 36px
+        '9.5': '2.375rem', // 38px
+        '20': '5rem', // 80px
+      },
+    },
+  },
+  plugins: [
+      require('@tailwindcss/typography'),
+  ],
+}
